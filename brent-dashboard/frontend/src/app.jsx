c28c5c7ed3d651048db8cb591482f12545493264
HanaTeshome721 <hana2729tm@gmail.com>
2025-08-05 19:26:17 +0300

    feat:basic set up

---
+++ brent-dashboard/frontend/src/app.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PriceChart from "./components/Pricechart";
+
+function App() {
+  return (
+    <div>
+      <h1>Brent Oil Price Change Dashboard</h1>
+      <PriceChart />
+    </div>
+  );
+}
+
+export default App;
